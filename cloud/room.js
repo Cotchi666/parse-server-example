2fa36b338e7461e4ff8674eb15e73d4f3612fbf7
Nguyễn Ngọc Chiến <56423606+Cotchi666@users.noreply.github.com>
2022-08-03 16:19:23 +0700

    update

--- cloud/room.js
+++ cloud/room.js
@@ -1,42 +1,34 @@
-// const { async } = require('parse/lib/browser/Storage');
 
-const { async } = require("parse/lib/browser/StorageController.browser");
+const { async } = require('parse/lib/browser/StorageController.browser');
 
 const Room = Parse.Object.extend('Room');
-const RentHouse = Parse.Object.extend('RentHouse');
 
+//authorized function
+// const validationRules = request => {
+//   if (request.master) {
+//     return;
+//   }
+//   if (!request.user || request.user.id !== 'masterUser') {
+//     throw 'Unauthorized';
+//   }
+// }
 
-Parse.Cloud.define('create-new-room',  req => {
+Parse.Cloud.define('create-new-room', req => {
   const room = new Room();
-  const name = req.params.name
-  var q = new Parse.Query("RentHouse");
+  const name = req.params.name;
+  var q = new Parse.Query('RentHouse');
   q.equalTo('objectId', 'VEKZbIMrDP');
   q.first().then(function (obj) {
-    console.log(obj.attributes)
-    room.set("parent", obj)
-   var a = room.set("name" , name)
-    console.log(a)
-     room.save();
-    })
-  
+    console.log(obj.attributes);
+    room.set('parent', obj);
+    room.set('name', name);
+    room.save();
+  });
   return 'ok';
 });
+// },validationRules);
 
-Parse.Cloud.afterSave('create-new-room', req => {
-  var room = req.object;
-  if (!room.existed()) {
-    console.log("thanh cong roi");
+Parse.Cloud.afterSave('create-new-room', function()  {
+  console.log("SOS")
   
-  }
-})
-// var q = new Parse.Query("RentHouse");
-// q.equalTo('objectId', 'VEKZbIMrDP');
-// q.first().then(function (obj) {
-//   console.log(obj.attributes)
-//   room.set("parent", obj.attributes)
-//   room.save(null, {
-//     success: function () {
-//       console.log("ok")
-//     }
-//   })
-// })
+});
