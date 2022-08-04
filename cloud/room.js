d3f40cfe299bcda3f1ad61c30807f02c9d48d009
Nguyễn Ngọc Chiến <56423606+Cotchi666@users.noreply.github.com>
2022-08-04 11:12:29 +0700

    update

--- cloud/room.js
+++ cloud/room.js
@@ -1,5 +1,5 @@
-
 const { query } = require('express');
+const { masterKey } = require('parse');
 const { async } = require('parse/lib/browser/StorageController.browser');
 
 const Room = Parse.Object.extend('Room');
@@ -21,24 +21,36 @@ Parse.Cloud.define('create-new-room', req => {
 
 //get room by name
 Parse.Cloud.define('get-room-byname', async req => {
-       
-          const name = req.params.name;
-          var query =  new Parse.Query(Room)
-          var room =  query.equalTo('name', name);
-          query.include('parent')
-          let result =  await room.find();
-         
-          
-          // for(let i = 0; i< result.length; i++){
-          //     var thisRoom = result[i];   
-          // }
-          // var c = thisRoom
+  const name = req.params.name;
+  var query = new Parse.Query(Room);
+  var room = query.equalTo('name', name);
+  query.include('parent');
+  let result = await room.find();
+
+  // for(let i = 0; i< result.length; i++){
+  //     var thisRoom = result[i];
+  // }
+  // var c = thisRoom
   return result;
 });
 
 //create a room when a customer order
 Parse.Cloud.define('create-ordered-room', async req => {
-       
- 
-return result;
+  return result;
+});
+
+//create a room when a customer order
+Parse.Cloud.define('delete-room-by-name', async req => {
+  //get data from client
+  const name = req.params.name;
+  //declare query
+  var query = new Parse.Query(Room);
+  //query
+  var room1 = query.equalTo('name', name);
+  //Retrieves at most one Parse.Object that satisfies this query.
+  var data = room1.first();
+  //Destroy
+  (await data).destroy();
+
+  return data;
 });
