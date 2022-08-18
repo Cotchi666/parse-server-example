df741643e37b650156b6b43e4f0ef9122dab7039
Nguyễn Ngọc Chiến <56423606+Cotchi666@users.noreply.github.com>
2022-08-18 15:01:25 +0700

    update

--- cloud/test.js
+++ cloud/test.js
@@ -6,18 +6,55 @@ Parse.Cloud.define('order', req => {
   const phone = req.params.phone;
   const email = req.params.email;
   const room_id = req.params.room_id;
+  const paymentMethod = req.params.paymentMethod;
+  const totalPrice = req.params.totalPrice;
   var q = new Parse.Query('Room');
   //query
   q.equalTo('objectId', room_id);
   q.first().then(function (obj) {
     console.log(obj.attributes);
 
-    // order.set('fullName', fullName);
+    order.set('fullName', fullName);
     order.set('email', email);
     order.set('phone', phone);
+    order.set('paymentMethod', paymentMethod);
     order.set('room_id', obj);
-
+    order.set('totalPrice', totalPrice);
     order.save();
   });
-  return 'Created order successfully!!';
+
+  return 'OK';
+});
+Parse.Cloud.define('payment', req => {
+  const order = new Order();
+  // const room_id = req.params.room_id;
+
+  // var q = new Parse.Query('Room');
+  // //query
+  // q.equalTo('objectId', room_id);
+  // q.first().then(function (obj) {
+  //   console.log(obj.attributes);
+
+  //   order.set('isPaid', true);
+  //   order.set('paidAt', Date.now());
+  //   order.save();
+  // });
+  // const order = new Order()
+  var q = new Parse.Query(Order);
+  const b = q.equalTo('objectId',"r1po6q4czp" )
+  const a =q.first().then((obj)=>{
+    obj.set("isPaid", true)
+    obj.save()
+  })
+  // const a = q.first()
+  console.log('checking',a)
+  
+  
+  return a;
+});
+Parse.Cloud.define('PAYPAL', (req, res) => {
+  const arr = []
+  var a = 'AaCSy0DbFkyV9Sq1WgPZEwbk_O8RMo49-2s-SNOmyrHonODbz-kY7YR-F53I5RnGNlMcNkBmLLbZmux3'
+  arr.push(a)
+  return arr
 });
